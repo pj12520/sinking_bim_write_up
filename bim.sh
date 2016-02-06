d9f1eeeea01a17a72ef8fa5bf3b8c4ecdc1fbd92
pj12520 <paul.jarvis@bristol.ac.uk>
2016-02-06 19:26:04 +0000

    Completed first draft of testing section

--- bim.sh
+++ bim.sh
@@ -6,4 +6,12 @@ gnuplot "../../sinking_bim_data/trunk/testing/aspect/for_pub.p"
 
 gnuplot "../../sinking_bim_data/trunk/testing/particle_int/for_pub.p"
 
+gnuplot "../../sinking_bim_data/trunk/aux_param_data/initial_height/for_pub.p"
+
+gnuplot "../../sinking_bim_data/trunk/aux_param2/truncation/for_pub.p"
+
+gnuplot "../../sinking_bim_data/trunk/aux_param3/n_interf/for_pub.p"
+
+gnuplot "../../sinking_bim_data/trunk/aux_param4/diff_step/for_pub.p"
+
 make
