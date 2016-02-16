0a6491623fce65e16ff0fe4a854b4e2770c9e248
pj12520 <paul.jarvis@bristol.ac.uk>
2016-02-16 10:49:45 +0000

    Started writing results section

--- bim.sh
+++ bim.sh
@@ -14,4 +14,8 @@ gnuplot "../../sinking_bim_data/trunk/aux_param3/n_interf/for_pub.p"
 
 gnuplot "../../sinking_bim_data/trunk/aux_param4/diff_step/for_pub.p"
 
+gnuplot "../../sinking_bim_data/trunk/float_sink2/D=2.2/Bo=2.5/viscos_rat=1/config_for_pub.p"
+
+gnuplot "../../sinking_bim_data/trunk/float_sink2/D=2.2/Bo=3.5/viscos_rat=1/config_for_pub.p"
+
 make
